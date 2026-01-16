7ad309f33b6fca7c0e6aac9a7dce553060bb8405
venkateswarlu <168259825+Mekala-venky@users.noreply.github.com>
2026-01-16 14:12:02 +0530

    Delete backend directory

--- backend/routers/Admin/Admin.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { verifyToken, checkRole } = require('../../auth/JWT');
-const adminController = require('../../controllers/Admin/AdminOverview');
-
-router.get("/dashboard-stats", verifyToken, checkRole('admin'), adminController.getDashboardStats);
-router.get("/user-stats", adminController.getDashboardStats);
-module.exports = router;
